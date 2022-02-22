70be5486f695b23a9a6895aee0c5c213ae46b933
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-02-22 12:06:36 +0600

    array

--- C_plus/C_plus.cpp
+++ C_plus/C_plus.cpp
@@ -1,12 +1,19 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 
 
 int main()
 {
     std::cout << "Hello World!\n";
-  
+    int c1[] = {1,2,3,4,5};
+    for (auto i : c1)
+    {
+        std::cout << "i: " << i << std::endl;
+    }
+    
+    
+   
+    std::cout << "c1: " << c1 << std::endl;
+    std::cout << "c1: " << sizeof(c1) << std::endl;
 }
 
 
