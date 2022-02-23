bbd1d921601f40e340033b77fddccc1d8c103a86
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-02-23 10:24:31 +0600

    перечислеиня

--- C_plus/C_plus.cpp
+++ C_plus/C_plus.cpp
@@ -1,10 +1,37 @@
 ﻿#include <iostream>
 
+enum class race
+{
+	orban,
+	witcher,
+	varior
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
-   
+    constexpr auto gamer = race::varior;	
+	switch (gamer)
+	{
+	case race::orban:
+		{
+			std::cout << "orban!\n";
+		}
+		break;
+	case race::witcher:
+		std::cout << "witcher!\n";
+		break;
+	case race::varior:
+		{
+			std::cout << "varior!\n";			
+		}
+		break;
+	default:
+		{
+			std::cout << "out\n";
+		}
+		break;
+	}
 }
 
 
