e194bf4782caacdf82d358fe2decce84f13645a0
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-03-05 15:52:41 +0600

    sd

--- C_plus/C_plus.cpp
+++ C_plus/C_plus.cpp
@@ -1,8 +1,21 @@
 ﻿#include <iostream>
 
+char petruchio(const char* shrew) 
+{
+	const char tesst = shrew[0];
+    return tesst;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";   
+    std::cout << "Hello World!\n";
+    int original = 100;
+    int& original_ref = original;
+    printf("original: %d\n", original);
+    printf("original_ref: %d\n", original_ref);
+
+    constexpr int new_value = 200;
+    original_ref = new_value;
 }
 
 
