1a5968dfd8d4d47ee679667bef94c095c78b765f
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-03-05 10:20:09 +0600

    ссылки и указатели

--- C_plus/C_plus.cpp
+++ C_plus/C_plus.cpp
@@ -1,8 +1,27 @@
 ﻿#include <iostream>
 
+struct person
+{
+    char name[256];
+    int year;
+    ~person()
+    {
+        std::cout << "destroed\n";
+    }
+};
+
+void add_year(person& per, int year)
+{
+    per.year = year;
+    std::cout << per.year << "\n";
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";   
-}
+    std::cout << "Hello World!\n";
+    person per{};
+    add_year(per, 33);
+    std::cout << per.year << "\n";
+ }
 
 
