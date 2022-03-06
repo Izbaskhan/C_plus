30ab90aac2a739127b75db49052582880b684ed6
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-03-06 11:38:16 +0600

    fdgdgd

--- C_plus/C_plus.cpp
+++ C_plus/C_plus.cpp
@@ -1,8 +1,40 @@
 ﻿#include <iostream>
 
+struct person
+{
+	int age{};
+};
+
+person p1{12}, p2{3},p3{5},p4{8};
+
+person persons[5]
+{
+	p1,p2,p3,p4
+};
+struct contract
+{
+	person contract_persons[10]{};
+	int index{};
+	void add(const person& el)
+	{
+		contract_persons[index] = el;
+		index++;
+	};
+};
+
+void form_company(contract &contract)
+{
+	for (const auto& person:persons)
+	{
+		contract.add(person);
+	}
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";   
+	contract contracts{};
+    std::cout << "Hello World!\n";
+	form_company(contracts);
 }
 
 
