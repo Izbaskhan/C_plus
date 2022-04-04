29335ff89ad343afb33f8aaabdc20db505ff2b28
Избасхан Ябеков <izbaskhan@yandex.kz>
2022-04-04 17:26:35 +0600

    реализация

--- C_plus/C_plus.cpp
+++ C_plus/C_plus.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream>
 
+void ref_type(int& x) {
+    printf("lvalue reference %d\n",x);
+}
+void ref_type(int&& x) {
+    printf("rvalue reference %d\n", x);
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";   
+    std::cout << "Hello World!\n"; 
+    auto x = 1;
+    ref_type(x);
+    ref_type(2);
+    ref_type(x+2);
 
 }
